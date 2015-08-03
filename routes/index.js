ed229e2872ab89ba296f25a1d4460a4565220078
Pedro Cruz <pcruz93@gmail.com>
2015-08-02 23:48:52 -0500

    Lista de preguntas

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-var quizController = require('../controllers/quiz_controller')
+var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+// Página de entrada (home page)
+router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
@@ -12,8 +12,9 @@ router.get('/author',function(req,res){
   res.render('author',{ title:'Quiz', nombre: 'Pedro Cruz (@pcruz93)'})
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
-
+// Definición de rutas de /quizes
+router.get('/quizes',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 module.exports = router;
